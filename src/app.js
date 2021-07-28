d1a1e2ebeec8bd91a0d8c31c6dad58ba40eb44b6
Kewon <48837736+kewon96@users.noreply.github.com>
2021-07-28 14:30:55 +0900

    모듈화

--- src/app.js
+++ src/app.js
@@ -1,55 +1,18 @@
 /**
- * ES6의 class문법을 사용해서 추상화
+ * 추상화에서 했던 것 처럼 한곳에 몰아서 하는 것보단 
+ * 기능 혹은, 역할별로 분할하는게 관리면에서 좋다.
+ * 
+ * Component를 가지고 Items를 정의
  */
 
-class Component {
-  $target;
-  $state;
+import Items from "./components/Items.js";
 
-  constructor($target) {
-    this.$target = $target;
-    this.setup();
-    this.render();
-  }
+class App {
+  constructor() {
+    const $app = document.querySelector('#app');
 
-  setup() {};
-  template() { return '' };
-  setEvent() {};
-
-  render() {
-    this.$target.innerHTML = this.template();
-    this.setEvent();
-  }
-
-  setState(newState) {
-    this.$state = { ...this.$state, ...newState };
-    this.render();
+    new Items($app);
   }
 }
 
-class App extends Component {
-  setup() {
-    this.$state = { items: [ 'item1', 'item2' ] };
-  }
-
-  template() {
-    const { items } = this.$state;
-
-    return `
-      <ul>
-        ${items.map((item) => `<li>${item}</li>`).join("")}
-      </ul>
-      <button id="append">추가</button>
-    `;
-  }
-
-  setEvent() {
-    this.$target.querySelector('button').addEventListener("click", () => {
-      const { items } = this.$state;
-
-      this.setState({ items: [...items, `item${items.length + 1}`] });
-    });
-  }
-}
-
-new App(document.querySelector('#app'));
+new App();
